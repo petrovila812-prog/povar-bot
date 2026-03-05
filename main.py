579e7d8c149c53482b6f1bdf9459193109ac51ca
petrovila812-prog <petrovila812@gmail.com>
2026-03-05 11:01:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import static_ffmpeg
+static_ffmpeg.add_paths()
+import yt_dlp
+
+# Встав ці налаштування замість старих ydl_opts
 ydl_opts = {
     'format': 'bestaudio/best',
+    'noplaylist': True,
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'outtmpl': 'song.%(ext)s',
+    'outtmpl': 'song.mp3', # Жорстко прописуємо назву файлу
     'nocheckcertificate': True,
-    'quiet': True,
-    'no_warnings': True,
-    'source_address': '0.0.0.0', # допомагає обійти блоки IP
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' 
 }
+
